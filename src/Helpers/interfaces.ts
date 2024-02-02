0865018677b039de69d39eb27002d110742f4c06
Brian Lacey <ss3teddy@gmail.com>
2024-02-02 00:12:00 -0800

    Added types required by TypeScript.

---
+++ src/Helpers/interfaces.ts
@@ -0,0 +1,52 @@
+export interface IbookList {
+    book_id: string
+    goodreads_book_id: string;
+    best_book_id: string;
+    work_id: string;
+    books_count: string;
+    isbn: string;
+    isbn13: string;
+    authors: string;
+    original_publication_year: string;     
+    original_title: string;   
+    title: string;
+    language_code: string;
+    average_rating: string;
+    ratings_count: string;
+    work_ratings_count: string;
+    work_text_reviews_count: string;        
+    ratings_1: string;
+    ratings_2: string;
+    ratings_3: string;
+    ratings_4: string;
+    ratings_5: string;
+    image_url: string;
+    small_image_url: string  
+}
+
+export interface IfavoritesList {
+    book_id: string;
+    goodreads_book_id: string;
+    best_book_id: string;
+    work_id: string;
+    books_count: string;
+    isbn: string;
+    isbn13: string;
+    authors: string;
+    original_publication_year: string;     
+    original_title: string;   
+    title: string;
+    language_code: string;
+    average_rating: string;
+    ratings_count: string;
+    work_ratings_count: string;
+    work_text_reviews_count: string;        
+    ratings_1: string;
+    ratings_2: string;
+    ratings_3: string;
+    ratings_4: string;
+    ratings_5: string;
+    image_url: string;
+    small_image_url: string  
+}
+
